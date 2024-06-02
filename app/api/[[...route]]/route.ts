b4a5aa96ad6fe418e2e2d7feebc1556d7820360c
Morgan-Wilkinson <46545893+Morgan-Wilkinson@users.noreply.github.com>
2024-06-02 19:54:38 -0400

    Commiting current workflow

---
+++ app/api/[[...route]]/route.ts
@@ -0,0 +1,14 @@
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
+import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
+
+import authors from "./authors";
+
+export const runtime = "edge";
+
+const app = new Hono().basePath("/api");
+
+app.route("/authors", authors);
+
+export const GET = handle(app);
+export const POST = handle(app);
